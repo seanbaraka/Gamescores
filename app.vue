ecb76f82138fff30179543c47e5e88f3c29d04e2
seanbaraka <seanbaraka@gmail.com>
2023-05-21 22:52:43 +0300

    fix: remove hardcoded firebase test values

--- app.vue
+++ app.vue
@@ -2,14 +2,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
 const fixtures = ref()
+const { public: { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId }} = useRuntimeConfig()
 const app = initializeApp({
-  apiKey: "AIzaSyC-QorOakQ9RJqPWS1VfetxXagrRiDhMZo",
-  authDomain: "artemis-messenger.firebaseapp.com",
-  projectId: "artemis-messenger",
-  storageBucket: "artemis-messenger.appspot.com",
-  messagingSenderId: "777259064933",
-  appId: "1:777259064933:web:5d250b5a825696f986708c",
-  measurementId: "G-ZW9WM3DH95"
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId
 });
 
 const db = getFirestore(app);
