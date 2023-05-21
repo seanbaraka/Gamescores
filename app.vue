afd3bcdbe89fd48199a26f9e5673103dd4092c6a
seanbaraka <seanbaraka@gmail.com>
2023-05-21 22:38:57 +0300

    initialize the project

---
+++ app.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+const fixtures = ref()
+const app = initializeApp({
+  apiKey: "AIzaSyC-QorOakQ9RJqPWS1VfetxXagrRiDhMZo",
+  authDomain: "artemis-messenger.firebaseapp.com",
+  projectId: "artemis-messenger",
+  storageBucket: "artemis-messenger.appspot.com",
+  messagingSenderId: "777259064933",
+  appId: "1:777259064933:web:5d250b5a825696f986708c",
+  measurementId: "G-ZW9WM3DH95"
+});
+
+const db = getFirestore(app);
+const collections = collection(db, 'fixtures');
+const docs = await getDocs(collections);
+fixtures.value = docs.docs.map(record => record.data())
+
+</script>
+<template>
+  <div>
+    <NuxtPage />
+  </div>
+</template>
