ec7ab80ce9421bd871923b2f9b7e723eebd23322
seanbaraka <seanbaraka@gmail.com>
2023-07-14 12:41:11 +0300

    update our privacy policy

---
+++ pages/privacy.vue
@@ -0,0 +1,105 @@
+<script setup lang="ts">
+useHead({
+    title: "Privacy Policy",
+});
+</script>
+<template>
+    <div class="section container">
+        <h1>PRIVACY POLICY</h1>
+        <h2>Introduction</h2>
+        <p>
+            This Privacy Policy document describes how Game Scores ("we," "us," "our")
+            collects, uses, and protects your data when you use our mobile application
+            ("the app"). Please read this Privacy Policy carefully to understand our
+            practices regarding your information and how we handle it.
+        </p>
+
+        <h2>Data Collection</h2>
+        <p>
+            Our app collects data necessary to ensure its optimal functionality. The
+            types of information that we may collect include:
+        </p>
+        <ol>
+            <li>
+                <strong>Personal Information:</strong> This can include your name, email
+                address, and other data that you voluntarily provide when registering on
+                our app.
+            </li>
+            <li>
+                <strong>Device Information:</strong> We may collect certain information
+                about your mobile device, such as your device model, operating system,
+                unique device identifier, and IP address.
+            </li>
+            <li>
+                <strong>Usage Information:</strong> We track how you interact with the
+                app, such as which features you use and how often.
+            </li>
+            <li>
+                <strong>Performance Data:</strong> This includes data related to the
+                performance of the app, such as crash reports and other diagnostic data.
+            </li>
+        </ol>
+        <p>
+            The data collected is primarily for the purpose of improving the app's
+            performance and providing a better user experience.
+        </p>
+
+        <h2>Data Usage</h2>
+        <p>We use the data we collect to:</p>
+        <ol>
+            <li>Provide and maintain our services.</li>
+            <li>Improve, personalize, and expand our services.</li>
+            <li>Understand and analyze how you use our services.</li>
+            <li>Develop new products, services, features, and functionality.</li>
+            <li>
+                Communicate with you, either directly or through one of our partners,
+                for customer service, to provide you with updates and other information
+                relating to the app.
+            </li>
+        </ol>
+
+        <h2>Data Protection</h2>
+        <p>
+            We implement a variety of security measures to maintain the safety of your
+            personal information. We maintain suitable physical, electronic, and
+            managerial procedures to safeguard and secure the information and data
+            stored on our system.
+        </p>
+
+        <h2>Data Sharing</h2>
+        <p>
+            Game Scores respects your privacy rights and does not share, sell, rent,
+            or trade any collected information with third parties for their
+            promotional purposes.
+        </p>
+
+        <h2>Your Rights</h2>
+        <p>
+            As a user of our app, you have the right to request access to your
+            personal data that we hold and to ask for correction if any data is
+            inaccurate. Furthermore, you have the right to request deletion of your
+            personal data at any given time. To make such a request, please contact us
+            at the contact information provided below.
+        </p>
+
+        <h2>Changes to this Policy</h2>
+        <p>
+            Game Scores reserves the right to amend this Privacy Policy at any time.
+            We will notify you of any changes by posting the new Privacy Policy on
+            this page. You are advised to review this Privacy Policy periodically for
+            any changes.
+        </p>
+
+        <h2>Contact Us</h2>
+        <p>
+            If you have any questions about this Privacy Policy, please contact us at:
+        </p>
+        <p>Game Scores<br />(Provide your contact details)</p>
+
+        <h2>Consent</h2>
+        <p>
+            By using our app, you hereby consent to our Privacy Policy and agree to
+            its terms.
+        </p>
+    </div>
+</template>
