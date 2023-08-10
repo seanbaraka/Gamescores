4dc19543804d7a3e7f5532291fda1db57ba734dd
seanbaraka <seanbaraka@gmail.com>
2023-08-10 19:47:44 +0300

    initialize rapid api host

---
+++ common/rapidapi.ts
@@ -0,0 +1,11 @@
+export const options = {
+    method: 'GET',
+    url: 'https://api-football-v1.p.rapidapi.com/v3/timezone',
+
+};
+
+export const BASE_URL= 'https://api-football-v1.p.rapidapi.com'
+export const RAPID_HEADERS = {
+    'X-RapidAPI-Key': '5891b85b85msha5d208d0d49c4eap1c32f4jsn4ec596359719',
+    'X-RapidAPI-Host': 'api-football-v1.p.rapidapi.com'
+}
