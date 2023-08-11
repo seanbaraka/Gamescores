b878878bada6b0533b797b2af1b018a1c6a60b6b
seanbaraka <seanbaraka@gmail.com>
2023-08-11 12:20:22 +0300

    add dayjs for dates formating

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth'],
+    modules: ['@nuxtjs/tailwindcss', '@sidebase/nuxt-auth', 'dayjs-nuxt'],
     css: ["~/assets/css/main.css" ],
     runtimeConfig: {
         public: {
