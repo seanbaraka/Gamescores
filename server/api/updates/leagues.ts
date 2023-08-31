288b0c6bab8cc7f78ae25afb6cf64f787ef61b75
seanbaraka <seanbaraka@gmail.com>
2023-08-31 14:39:16 +0300

    refactor leagues response

--- server/api/updates/leagues.ts
+++ server/api/updates/leagues.ts
@@ -3,11 +3,11 @@ import {getLeagues} from "~/common/api";
 export default defineEventHandler(async(event) => {
     // Retrieves a list of all available leagues.
     const leagues: any[] = await getLeagues();
-    return leagues.map((league) => ({
-        id: league.league.id,
-        name: league.league.name,
-        country: league.country.name,
-        type: league.league.type,
-        logo: league.league.logo,
+    const results: any[] = leagues.map((league) => ({
+        id: league.id,
+        name: league.name,
+        country: league.country,
+        logo: league.logo,
     }));
+    return results;
 })
