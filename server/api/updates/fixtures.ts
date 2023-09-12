255062a42a6941fc9444def5dc512f2bee63437b
seanbaraka <seanbaraka@gmail.com>
2023-09-12 16:47:02 +0300

    retrieve fixtures from cache

--- server/api/updates/fixtures.ts
+++ server/api/updates/fixtures.ts
@@ -1,24 +1,25 @@
-import {getFixtures} from "~/common/api";
-import {LEAGUES} from "~/common/rapidapi";
-import dayjs from 'dayjs'
+import { getUpdatesFromCache } from '~/common/api';
+import dayjs from 'dayjs';
 
 export default defineEventHandler(async (event) => {
-    const query = getQuery(event);
-    const params = { date: dayjs(query.date as string).format('YYYY-MM-DD') }
-    // get the fixtures
-    let matches: any[] = [];
-    for (let i = 0; i < LEAGUES.length; i++) {
-        const fixtures = await getFixtures(params, LEAGUES[i]);
-        matches = [...matches, ...fixtures];
-    }
-    return matches.map((fx: any) => ({
-        id: fx.fixture.id,
-        timestamp: fx.fixture.timestamp * 1000,
-        teams: fx.teams,
-        league: {
-            id: fx.league.id,
-            name: fx.league.name,
-            logo: fx.league.logo
-        }
-    }));
-})
+  const query = getQuery(event);
+  const params = {
+    date: dayjs(query.date as string).format('YYYY-MM-DD'),
+    league: query.league,
+  };
+  // get the fixtures
+  const date = dayjs(Date.now()).format('YYYY-MM-DD');
+
+  return await getUpdatesFromCache(date);
+
+  // return matches.map((fx: any) => ({
+  //   id: fx.fixture.id,
+  //   timestamp: fx.fixture.timestamp * 1000,
+  //   teams: fx.teams,
+  //   league: {
+  //     id: fx.league.id,
+  //     name: fx.league.name,
+  //     logo: fx.league.logo,
+  //   },
+  // }));
+});
