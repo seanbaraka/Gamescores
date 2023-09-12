c1e565d0e700cfa51bb5d6b3d50eda32c99c464e
seanbaraka <seanbaraka@gmail.com>
2023-09-12 16:46:37 +0300

    handle fixture updates logic

--- server/api/updates/fixture-update.ts
+++ server/api/updates/fixture-update.ts
@@ -1,11 +1,17 @@
 import { H3Event } from 'h3';
+import {updateFixtures} from "~/common/api";
 
 export default defineEventHandler(async (event: H3Event) => {
   const method = getMethod(event);
   if (method !== 'POST') {
     return;
   }
-  const { selectedFixtures } = await readBody(event);
-  console.log(selectedFixtures);
-  return selectedFixtures;
+  const { selectedFixtures, date } = await readBody(event);
+  try {
+    await updateFixtures(selectedFixtures, date);
+    return { success: true }
+  } catch (e: any) {
+    console.log('Could not update fixtures. Reason', e.message);
+    createError({ statusCode: 400, statusMessage: 'Could not update fixtures '});
+  }
 });
