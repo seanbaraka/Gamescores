46e1f73eb9e48fdd474db6ce42959119d3a0e91d
seanbaraka <seanbaraka@gmail.com>
2023-09-14 17:10:10 +0300

    changed the nuxt ui version

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ['@sidebase/nuxt-auth', 'dayjs-nuxt', '@nuxthq/ui'],
+    modules: ['@sidebase/nuxt-auth', 'dayjs-nuxt', '@nuxt/ui'],
     colorMode: {
         preference: 'light'
     },
