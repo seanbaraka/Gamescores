1fa53a40d97e3ba5c014d1d593a2f62451dbcdd5
michael njuguna <michaelnjuguna184@gmail.com>
2023-10-19 17:23:18 +0300

    removed config.json

--- server/db/data-source.ts
+++ server/db/data-source.ts
@@ -5,16 +5,16 @@ import { User } from './models/User';
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.PG_HOST,
-  port: 6434,
-  username: 'postgres',
+  port: Number(process.env.PG_PORT),
+  username: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
-  database: 'railway',
+  database: process.env.PG_DATABASE_NAME,
   synchronize: true,
   logging: true,
 //   entities hold models
   entities: [User],
   subscribers: [],
   migrations: [],
-});
+}); 
 
 
