eda6294ce21b661057c345b7e629d6151e314798
seanbaraka <seanbaraka@gmail.com>
2023-10-23 18:51:04 +0300

    default page

---
+++ pages/hey.vue
@@ -0,0 +1,13 @@
+<template>
+    <section class="h-screen flex items-center justify-center">
+        <div class="container text-center">
+            <Logo class="text-center inline-block my-4 w-36" />
+            <h2 class="text-center font-semibold text-[4rem]">Yooh, how are you ?</h2>
+            <p>Your home of live football updates from over 500+ leagues across the globe</p>
+            <a href="https://play.google.com/store/apps/details?id=com.gamescores.app" target="_blank"
+                class="inline-block py-6 cursor-pointer underline text-green-500 font-semibold">
+                Check us Out
+            </a>
+        </div>
+    </section>
+</template>
