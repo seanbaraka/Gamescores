f8b9ef403583c0fce4687b0e9ce3d4b159348a6b
seanbaraka <seanbaraka@gmail.com>
2023-10-23 14:24:39 +0300

    changed the payload & removed extra query to db

--- server/api/users/create-user.ts
+++ server/api/users/create-user.ts
@@ -1,21 +1,16 @@
 // save a new user into db using typeorm
-import { AppDataSource } from "~/server/db/data-source"
-import {User} from '~/server/db/models/User';
+import { AppDataSource } from '~/server/db/data-source';
+import { User } from '~/server/db/models/User';
 
+export default defineEventHandler(async (event) => {
+  const userInfo = await readBody(event);
+  const repo = AppDataSource.getRepository(User);
+  const newUser = repo.create(userInfo);
+  const userRecord = await repo.save(newUser);
 
-  export default defineEventHandler(async (event) => {
-    const userInfo =await readBody(event);
-    const repo = AppDataSource.getRepository(User);
-    const newUser = repo.create({
-          email: userInfo.email,
-            role: userInfo.role,
-            isPremium: userInfo.isPremium,
-            active: userInfo.active
-    });
-    await repo.save(newUser);
-    if(await repo.findOneBy({email: userInfo.email})){
-      return {status: 201, data: await repo.findOneBy({email: userInfo.email})}
-    }else{
-      return {status: 400, data: "User not added"}
-    }
+  if (userRecord) {
+    return { status: 201, data: userRecord };
+  } else {
+    return { status: 400, data: 'User not added' };
+  }
 });
