7a50bbfc12113927f9a76b26d6ef8d99da3da37f
seanbaraka <seanbaraka@gmail.com>
2023-10-23 18:52:00 +0300

    ui tweaks

--- app.vue
+++ app.vue
@@ -1,22 +1,22 @@
 <script setup lang="ts">
-import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
-const fixtures = ref()
-const { public: { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId }} = useRuntimeConfig()
-const app = initializeApp({
-  apiKey,
-  authDomain,
-  projectId,
-  storageBucket,
-  messagingSenderId,
-  appId,
-  measurementId
-});
+// import { initializeApp } from "firebase/app";
+// import { getFirestore, collection, getDocs } from "firebase/firestore/lite";
+// const fixtures = ref()
+// const { public: { apiKey, authDomain, projectId, storageBucket, messagingSenderId, appId, measurementId }} = useRuntimeConfig()
+// const app = initializeApp({
+//   apiKey,
+//   authDomain,
+//   projectId,
+//   storageBucket,
+//   messagingSenderId,
+//   appId,
+//   measurementId
+// });
 
-const db = getFirestore(app);
-const collections = collection(db, 'fixtures');
-const docs = await getDocs(collections);
-fixtures.value = docs.docs.map(record => record.data())
+// const db = getFirestore(app);
+// const collections = collection(db, 'fixtures');
+// const docs = await getDocs(collections);
+// fixtures.value = docs.docs.map(record => record.data())
 
 
 
