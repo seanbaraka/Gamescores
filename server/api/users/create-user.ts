bdde29c5ed6d48b4d33defaffe9c0ecf1203a097
seanbaraka <seanbaraka@gmail.com>
2023-10-23 17:55:21 +0300

    fix

--- server/api/users/create-user.ts
+++ server/api/users/create-user.ts
@@ -1,10 +1,9 @@
 // save a new user into db using typeorm
-import { AppDataSource } from '~/server/db/data-source';
 import { User } from '~/server/db/models/User';
 
 export default defineEventHandler(async (event) => {
   const userInfo = await readBody(event);
-  const repo = AppDataSource.getRepository(User);
+  const repo = User.getRepository();
   const newUser = repo.create(userInfo);
   const userRecord = await repo.save(newUser);
 
